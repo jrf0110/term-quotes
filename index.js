a8a2a4eba4aa22f45526867d12677f48b6b1e57e
John Fawcett <jrf0110@gmail.com>
2012-07-31 12:42:55 -0500

    added addy quote

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var
 , quotes = [
     { quote: "The secret to building large apps is never build large apps. Break your applications into small pieces. Then, assemble those testable, bite-sized pieces into your big application"
     , author: "Justin Meyer" }
+
   , { quote: "The key is to acknowledge from the start that you have no idea how this will grow. When you accept that you don't know everything, you begin to design the system defensively. You identify the key areas that may change, which often is very easy when you put a little bit of time into it. For instance, you should expect that any part of the app that communicates with another system will likely change, so you need to abstract that away."
     , author: "Nicholas Zakas" }
+
   , { quote: "The more tied components are to each other, the less reusable they will be, and the more difficult it becomes to make changes to one without accidentally affecting another"
     , author: "Rebecca Murphey" }
+
+  , { quote: "By maintaining a consistent facade (simplified API), the worry about whether a module extensively uses dojo, jQuery, YUI, zepto or something else becomes significantly less important. As long as the interaction layer doesn't change, you retain the ability to switch out libraries (eg. jQuery for Dojo) at a later point without affecting the rest of the system."
+    , author: "Addy Osmani" }
   ]
 , curr   = 0
 , onTick = function(){
